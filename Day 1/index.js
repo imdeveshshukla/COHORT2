5fa7fd77e11ffbfea38516422b9228c65016b8b2
Devesh Shukla <deveshshukla1603@gmail.com>
2023-12-09 18:07:12 +0530

    Added Some

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,3 +1,11 @@
+const fs = require('fs');
+
+//-----------------------Example of Async Code
+fs.readFile('a.txt',"utf-8",function(err, data) {
+    console.log(data);
+})
+//-----------------------
+
 console.log("Hello");
 let sum  = 0;
 // for(let i = 0;i<10000000000000000;i++) //takes to to to much time

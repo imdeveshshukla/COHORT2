7f1d48f34f46ec8289616bd51d8b14f856b7c1c0
Devesh Shukla <deveshshukla1603@gmail.com>
2024-01-06 23:07:49 +0530

    react

---
+++ week6/week6_experiment/src/App.jsx
@@ -0,0 +1,39 @@
+const { useState } = require("react")
+
+function App(){
+  
+  return(
+    <>
+      <HeaderWithButton></HeaderWithButton>
+      <Header title="devesh2"></Header>
+      <Header title="devesh2"></Header>
+      <Header title="devesh2"></Header>           /* here we sending same parameters so memo does not rerendering*/
+    </>
+  )
+}
+
+
+function HeaderWithButton(){          //HeaderWithButton is only rerendering not app component
+  const {title,updateTitle} = useState("Devesh");
+  
+  function setTitle(){
+    updateTitle("My name is "+Math.random());
+  }
+  return(
+    <>
+    <button onClick={setTitle}></button>
+    <Header title={title}></Header>
+    </>
+  )
+}
+
+
+
+const Header = React.memo(function Header({title}){    //we can also write (props) and then props.title
+  return(
+    <>
+      {title}
+    </>
+  )
+})  //when we pass same parameters and learn about it 
+

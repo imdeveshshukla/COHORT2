38446bdb50e4ec7e96002fca5cfe66cf2151d996
Devesh Shukla <deveshshukla1603@gmail.com>
2024-01-20 21:18:34 +0530

    Tailwind

---
+++ week7/week7Exp/src/Context.jsx
@@ -0,0 +1,7 @@
+import { createContext } from "react";
+const count = 0;
+const setCount = null
+export const CountContext = createContext({
+    count,
+    setCount
+});
---
+++ week8/week8Exp/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './index.css'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='grid grid-cols-1 md:grid-cols-3'>
+      <div className='bg-red-800 h-10'></div>
+      <div className='bg-blue-800 h-10'></div>
+      <div className='bg-green-800 h-10'></div>
+      <div className='bg-blue-800 h-10'></div>
+      <div className='bg-green-800 h-10'></div>
+      <div className='bg-red-800 h-10'></div>
+    </div>
+  )
+}
+
+export default App

a719e4a6b4fddf31e3a92a3fb2f1b0efc78cb47e
Devesh Shukla <deveshshukla1603@gmail.com>
2024-02-19 22:50:18 +0530

    week7

--- week7/Assignments/Assignments/src/App.jsx
+++ week7/Assignments/Assignments/src/App.jsx
@@ -1,13 +1,21 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 import { data } from './Data/Details'
 import Assignment1 from './components-Assignment/Assignment1'
 import Assignment2 from './components-Assignment/Assignment2'
 import Assignment3 from './components-Assignment/Assignement3'
+import Assignment4 from './components-Assignment/Assignment4'
 function App() {
   const [count, setCount] = useState(0)
   const [updup, hey] = useState("");
 
+  const [profile,setProf] = useState(null);
+  useEffect(() => {
+      fetch("https://api.github.com/users/imdeveshshukla")
+      .then(res => res.json())
+      .then(d => setProf(d))
+  }, [])
+  
   return (
     <div className='outerItems'>
     {/* <div className='items'>
@@ -15,7 +23,9 @@ function App() {
     </div>
       <Assignment2/>*/}
       
-      <Assignment3/> 
+      {/* <Assignment3/>  */}
+    
+      <Assignment4 profile={profile}/>
 
     </div>
   )
